1908fa04295da86e72d97bcb750493e9f1d94646
philb <philboyd72@gmail.com>
2016-07-30 15:19:09 -0400

    Added wordlist.json and ways to get theWord

---
+++ mobile/src/main/java/kylestrait/wordoftheday/Word.java
@@ -0,0 +1,28 @@
+package kylestrait.wordoftheday;
+
+/**
+ * Created by philb on 7/30/2016.
+ */
+public class Word {
+    private String name;
+    private String definition;
+
+    public Word() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+}
---
+++ mobile/src/main/java/kylestrait/wordoftheday/WordOfMoment.java
@@ -0,0 +1,60 @@
+package kylestrait.wordoftheday;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
+
+/**
+ * Created by philb on 7/30/2016.
+ */
+public class WordOfMoment {
+
+    private Word theWord;
+    private Context context;
+    private Random random;
+
+    public WordOfMoment(Context context) {
+        this.context = context;
+        random = new Random();
+        setTheWord();
+    }
+
+    public Word getTheWord() {
+        return theWord;
+    }
+
+    public void setTheWord() {
+        String json = null;
+        try {
+            InputStream is = context.getAssets().open("providers.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        try {
+            JSONObject obj = new JSONObject(json);
+            JSONArray m_jArry = obj.getJSONArray("wordlist");
+
+            int randomNumber = random.nextInt(m_jArry.length());
+
+            JSONObject jo_inside = m_jArry.getJSONObject(randomNumber);
+            theWord.setName(jo_inside.getString("WORD"));
+            theWord.setDefinition(jo_inside.getString("DEFINITION"));
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
