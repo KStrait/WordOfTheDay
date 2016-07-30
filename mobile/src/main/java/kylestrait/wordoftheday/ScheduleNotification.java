0ec297c54038b48e5bf6139f60a158067519e812
KStrait <KStrait@umflint.edu>
2016-07-30 19:11:06 -0400

    Initial setup

--- mobile/src/main/java/kylestrait/wordoftheday/ScheduleNotification.java
+++ mobile/src/main/java/kylestrait/wordoftheday/ScheduleNotification.java
@@ -29,8 +29,8 @@ public class ScheduleNotification{
 // Set the alarm to start at approximately 12:00 p.m.
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.set(Calendar.HOUR_OF_DAY, 18);
-        calendar.set(Calendar.MINUTE, 55);
+        calendar.set(Calendar.HOUR_OF_DAY, 19);
+        calendar.set(Calendar.MINUTE, 15);
 
 // With setInexactRepeating(), you have to use one of the AlarmManager interval
 // constants--in this case, AlarmManager.INTERVAL_DAY.
